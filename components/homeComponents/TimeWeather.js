75005c11a5276589ab4ffd02a3fa9f1911ba1c88
Vachek-hooves <vachek0602@gmail.com>
2025-01-16 17:14:35 +0200

    update

---
+++ components/homeComponents/TimeWeather.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const TimeWeather = () => {
+  return (
+    <View>
+      <Text>TimeWeather</Text>
+    </View>
+  );
+};
+
+export default TimeWeather;
+
+const styles = StyleSheet.create({});
