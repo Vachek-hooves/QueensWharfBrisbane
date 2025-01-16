4d92465615f17e1f10a93719176393cf99e6a4b9
Vachek-hooves <vachek0602@gmail.com>
2025-01-16 17:20:01 +0200

    create map component

--- allScreens/Map.js
+++ allScreens/Map.js
@@ -1,14 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import {StyleSheet, View, Image, Dimensions} from 'react-native';
+import React from 'react';
+import MapView, {Marker, PROVIDER_DEFAULT} from 'react-native-maps';
+import {useAppStore} from '../store/context';
+import {mainLocation} from '../data/location';
 const Map = () => {
+  const {places} = useAppStore();
+
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_DEFAULT}
+        style={styles.map}
+        initialRegion={mainLocation}>
+        {places.map(place => (
+          <Marker
+            key={place.id}
+            coordinate={{
+              latitude: place.location.latitude,
+              longitude: place.location.longitude,
+            }}
+            title={place.name}
+            description={place.place}>
+            <View style={styles.markerContainer}>
+              <Image
+                source={{uri: place.image}}
+                style={styles.markerImage}
+                resizeMode="cover"
+              />
+            </View>
+          </Marker>
+        ))}
+      </MapView>
     </View>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  markerContainer: {
+    backgroundColor: '#001F1F',
+    borderRadius: 20,
+    padding: 2,
+    borderWidth: 2,
+    borderColor: '#00AAB8',
+  },
+  markerImage: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+  },
+});
---
+++ data/location.js
@@ -0,0 +1,6 @@
+export const mainLocation = {
+  latitude: -27.47,
+  longitude: 153.02,
+  latitudeDelta: 0.1,
+  longitudeDelta: 0.1,
+};
